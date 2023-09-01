7e9733389e79009f60088dec7ea22fd0599e73b2
Marcelino <marcelinomeba@hotmail.com>
2023-09-01 17:21:56 -0500

    Configurando los CORS para la peticiones la backend

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
 const clientAxios = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
+    baseURL: import.meta.env.VITE_API_URL,
+    headers: {
+        'Accept' : 'application/json',
+        'X-Resquest-Whit': 'XMLHttRequest'
+    },
+    withCredentials: true
 });
 
 export default clientAxios;

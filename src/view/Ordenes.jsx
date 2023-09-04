0a040cfd9031e7c68f4eedef7fdecfbfb72de8ba
Marcelino <marcelinomebe@gmail.com>
2023-09-04 15:39:40 -0500

    Mostrando el Contenido del Pedido

--- src/view/Ordenes.jsx
+++ src/view/Ordenes.jsx
@@ -10,15 +10,41 @@ export default function Ordenes() {
     })
 
     const { data, error, isLoading } = useSWR('/api/pedidos', fetcher);
-    console.log(data?.data);
-    console.log(error);
-    console.log(isLoading);
+    
+    if (isLoading) return 'Cargando...'
     return (
         <div>
             <h1 className="text-4xl font-black">Ordenes</h1>
             <p className="text-2xl my-10">
                 Administra las ordenes desde aqui
             </p>
+
+            <div>
+                {data.data.data.map(pedido => (
+                    <div 
+                        key={pedido.id} 
+                        className="p-5 bg-white shadow space-y-2 border-b"
+                    >
+                        <p>
+                            Contenido del Pedido:
+                        </p>
+
+                        {pedido.productos.map(producto => (
+                            <div
+                                key={producto.id}
+                                className="border-b border-b-slate-200 last-of-type:border-none py-4"
+                            >
+                                <p className='text-sm'>ID: {producto.id} </p>
+                                <p className='text-sm'>{producto.nombre} </p>
+                                <p>
+                                    Cantidad: {''}
+                                    <span className='font-bold'>{producto.pivot.cantidad}</span>
+                                </p>
+                            </div>
+                        ))}
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }

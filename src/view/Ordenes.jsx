b23a5934bd53da626a627d15e5f327278f073621
Marcelino <marcelinomebe@gmail.com>
2023-09-04 15:13:35 -0500

    Añadir los Productos del Pedido en el JSON

--- src/view/Ordenes.jsx
+++ src/view/Ordenes.jsx
@@ -1,6 +1,18 @@
-import React from 'react'
+import useSWR from 'swr'
+import clientAxios from '../config/axios'
 
 export default function Ordenes() {
+    const token = localStorage.getItem('AUTH_TOKEN')
+    const fetcher =  () => clientAxios('/api/pedidos', {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    })
+
+    const { data, error, isLoading } = useSWR('/api/pedidos', fetcher);
+    console.log(data?.data);
+    console.log(error);
+    console.log(isLoading);
     return (
         <div>
             <h1 className="text-4xl font-black">Ordenes</h1>
